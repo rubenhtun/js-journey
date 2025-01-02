e7dae0cceb344445e671f1864cb8957ccf704124
RubenHtun <rubenhtun2003@gmail.com>
2025-01-02 14:41:04 +0700

    chore: updated some js features

--- 0014-Movie-Search-App/app.js
+++ 0014-Movie-Search-App/app.js
@@ -5,8 +5,18 @@ const results = document.getElementById("results");
 
 const Mov_Web_API_KEY = "7375868b"; // omdbapi.com မှ API key ကို ထည့်အသုံးပြုပါမည်။
 
+// အကယ်၍ user သည် search input တွင် Enter Key နှိပ်ပြီးရှာခဲ့သော်
+searchInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    fetchMovies();
+  }
+});
+
 // Search Button ကို နှိပ်သောအခါ callback function တစ်ခုကိုအသုံးပြုပြီး data fetch လုပ်ပါမည်။
-searchButton.addEventListener("click", async () => {
+searchButton.addEventListener("click", () => fetchMovies());
+
+const fetchMovies = async () => {
   const queryValue = searchInput.value.trim();
   if (queryValue) {
     // searchInput မှ input term မှန်ကန်မှသာလျှင် fetch လုပ်နိုင်ပါမည်။
@@ -17,26 +27,27 @@ searchButton.addEventListener("click", async () => {
 
       if (!response.ok) throw new Error("Failed to fetch DATA!"); // server response အဆင်မပြေဖြစ်ခဲ့ရင်
       const movieData = await response.json();
-      console.log(movieData);
 
       if (movieData.Response === "True") {
-        movieDisplay(movieData.Search); // movies အချက်အလက်များကို ပြသနိုင်ရန် response movieData ထဲကမှ Search ဆိုတဲ့ array object ကို ခေါ်ရ
+        movieDisplay(movieData.Search); // movies အချက်အလက်များကို ပြသနိုင်ရန် response movieData ထဲကမှ Search ဆိုတဲ့ array object ကို ရယူပါသည်။
       } else {
         results.innerHTML = `<p class="text-red-500">"${queryValue}" ဆိုသည့်ရလဒ်မရှိပါ!</p>`;
       }
     } catch (err) {
       console.log("Error: ", err);
-      results.innerHTML = `<p class="text-red-500">အမှားတစ်ခုဖြစ်ပွားနေပါသည်: ${err.message}</p>`;
+      results.innerHTML = `<p class="text-red-500">အမှားယွင်းတစ်ခုဖြစ်နေပါသည်: ${err.message}</p>`;
     }
   } else {
     results.innerHTML = `<p class="text-red-500">ကျေးဇူးပြု၍ ရှာဖွေရန်စကားလုံးရိုက်ထည့်ပါ။</p>`;
   }
-});
+};
 
 // movies များကို display လုပ်ရန် function တစ်ခုကို တည်ဆောက်ပါမည်။
 const movieDisplay = (movies) => {
+  // movieData.Seach မှ arguments များသည် movies parameter သို့ လက်ခံရရှိပါမည်။
   results.innerHTML = movies
     .map(
+      // map method ကို အသုံးပြုထားပါသည်။
       (movie) => `
     <div class="bg-white shadow-md rounded p-4">
       <img
