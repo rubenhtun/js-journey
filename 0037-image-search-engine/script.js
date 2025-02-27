8442762972fa5359d10779a26cadf33784a37871
Ruben Htun <rubenhtun2003@gmail.com>
2025-02-27 12:04:48 +0700

    modified inner functions

--- 0037-image-search-engine/script.js
+++ 0037-image-search-engine/script.js
@@ -1,23 +1,34 @@
-async function searchImages() {
-  const query = document.getElementById("search-input").value;
-  const resultsContainer = document.getElementById("image-results");
-  resultsContainer.innerHTML = "";
+const search = document.getElementById("search-input");
+
+search.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    searchImage();
+  }
+});
+
+const searchImage = async () => {
+  const query = search.value;
+  const resultedImagesList = document.getElementById("resulted-images-list");
 
   if (query.trim() === "") {
     alert("Please enter a search term");
     return;
   }
 
+  // Fetching data from unsplash API
   const response = await fetch(
-    `https://api.unsplash.com/search/photos?query=${query}&client_id=xKLwAFHBeU-A37eHENdQYJUAAe7Z_7BOs54aWebF2j0`
+    "https://api.unsplash.com/search/photos?query=" +
+      query +
+      "&client_id=xKLwAFHBeU-A37eHENdQYJUAAe7Z_7BOs54aWebF2j0"
   );
-  const data = await response.json();
 
-  data.results.forEach((image) => {
-    const imgElement = document.createElement("img");
-    imgElement.src = image.urls.small;
-    imgElement.alt = image.alt_description;
-    imgElement.classList.add("rounded-lg", "shadow-md", "w-full");
-    resultsContainer.appendChild(imgElement);
+  const images = await response.json();
+
+  images.results.forEach((image) => {
+    const imageCardElement = document.createElement("img");
+    imageCardElement.src = image.urls.small;
+    imageCardElement.alt = image.alt_description;
+    imageCardElement.classList.add("rounded-lg", "shadow-md", "w-full");
+    resultedImagesList.appendChild(imageCardElement);
   });
-}
+};
