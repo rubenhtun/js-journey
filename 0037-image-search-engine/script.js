99d1e6ca2893717a7ebef146f56939db39532f2c
Ruben Htun <rubenhtun2003@gmail.com>
2025-06-05 12:21:40 +0700

    feat: added comment line

--- 0037-image-search-engine/script.js
+++ 0037-image-search-engine/script.js
@@ -21,7 +21,7 @@ const searchImage = async () => {
 
   // Fetch images from Unsplash API
   const response = await fetch(
-    "https://api.unsplash.com/search/photos?query=" + query + "&client_id="
+    "https://api.unsplash.com/search/photos?query=" + query + "&client_id=" // Add your Unsplash API access key
   );
 
   const images = await response.json();
